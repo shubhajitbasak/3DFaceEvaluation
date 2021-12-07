a7b4fd81d5f920cc6b0d508b85e2a3c121761a68
shubhajitbasak <shubhajit.basak@yahoo.com>
2021-12-07 13:05:11 +0000

    Git Code Commit

---
+++ Data/MICCFlorence/BFM/createljson.py
@@ -0,0 +1,41 @@
+import json
+import numpy as np
+
+with open('../../Test/110920150452.ljson') as f1:
+    data = json.load(f1)
+
+# s = data['groups']['LJSON']['landmarks']['points']
+
+# loc = np.load('landmark_ids_loc.npy').tolist()
+
+data['groups']['LJSON']['landmarks']['points'] = np.load('landmark_ids_loc.npy').tolist()
+
+with open('../../Test/landmark_ids_bfm.ljson', 'w') as f2:
+    json.dump(data, f2, indent=4)
+
+# import menpo3d.io as m3io
+# import menpo.io as mio
+# from menpo.shape import PointCloud
+# from menpo.transform import AlignmentSimilarity
+# from menpo3d.correspond import nicp
+#
+# templateObj = '../../Test/bfm_model_front.obj'
+# templateLandmarkPkl = '../../Test/landmark_ids_bfm.pkl'
+# templateLandmarkLjson = '../../Test/landmark_ids_bfm.ljson'
+# sourceObj = '../../Test/110920150452.obj'
+# sourceLandmarkLjson = '../../Test/110920150452.ljson'
+# exportPath = '../../Test/110920150452_reg.obj'
+#
+# template = m3io.import_mesh(templateObj)
+# # template_lms = np.array(mio.import_pickle(templateLandmarkPkl))
+# # template.landmarks['LJSON'] = PointCloud(template.points[template_lms])
+# template.landmarks['LJSON'] = m3io.import_landmark_file(templateLandmarkLjson)['LJSON']
+#
+# source = m3io.import_mesh(sourceObj)
+# source.landmarks['LJSON'] = m3io.import_landmark_file(sourceLandmarkLjson)['LJSON']
+#
+# lm_align = AlignmentSimilarity(source.landmarks['LJSON'], template.landmarks['LJSON']).as_non_alignment()
+# source = lm_align.apply(source)
+# # m3io.export_mesh(source, 'Test/bfm_aligned.obj')
+# result = nicp.non_rigid_icp(template, source, landmark_group='LJSON', verbose=True)
+# m3io.export_mesh(result, exportPath, overwrite=True)
--- MICC_Model_Registration.py
+++ MICC_Model_Registration.py
@@ -1,3 +1,6 @@
+import glob
+import os.path
+
 import numpy as np
 
 import menpo3d.io as m3io
@@ -5,6 +8,50 @@ import menpo.io as mio
 from menpo.shape import PointCloud
 from menpo.transform import AlignmentSimilarity
 from menpo3d.correspond import nicp
+
+
+def meshRegistration(templateObj, templateLandmarkPkl, sourceObj, sourceLandmarkLjson, exportPath):
+    template = m3io.import_mesh(templateObj)
+    template_lms = np.array(mio.import_pickle(templateLandmarkPkl))
+    template.landmarks['LJSON'] = PointCloud(template.points[template_lms])
+
+    source = m3io.import_mesh(sourceObj)
+    source.landmarks['LJSON'] = m3io.import_landmark_file(sourceLandmarkLjson)['LJSON']
+
+    lm_align = AlignmentSimilarity(source.landmarks['LJSON'], template.landmarks['LJSON']).as_non_alignment()
+    source = lm_align.apply(source)
+    # m3io.export_mesh(source, 'Test/bfm_aligned.obj')
+    result = nicp.non_rigid_icp(template, source, landmark_group='LJSON', verbose=True)
+    m3io.export_mesh(result, exportPath, overwrite=True)
+
+
+templateObj = 'Data/MICCFlorence/BFM/bfm_model_front.obj'
+templateLandmarkPkl = 'Data/MICCFlorence/BFM/landmark_ids_bfm.pkl'
+sourceObjRoot = 'Data/MICCFlorence/Reconstruction/GroundTruth/'  # 110920150452.obj'
+# sourceLandmarkLjson = 'Data/MICCFlorence/MICC_landmarks/subject_01/Model/frontal1/obj/110920150452.ljson'
+exportPathRoot = 'Data/MICCFlorence/Reconstruction/BFMRegistered'
+
+# meshRegistration(templateObj, templateLandmarkPkl, sourceObj, sourceLandmarkLjson, exportPath)
+
+for i in range(1, 54):
+    sourceObj = None
+    sourceLandmarkLjson = None
+    exportPath = None
+    # print(f"subject_{i:02d}")
+    sourceObjPath = os.path.join(sourceObjRoot, f"subject_{i:02d}")
+    sourceObjFile = glob.glob1(sourceObjPath, '*.obj')
+    sourceObj = os.path.join(sourceObjPath, sourceObjFile[0])
+    sourceLandmarks = glob.glob(f'Data/MICCFlorence/MICC_landmarks/subject_{i:02d}/**/*.ljson', recursive=True)
+    for lnd in sourceLandmarks:
+        if sourceObjFile[0].split('.')[0] == lnd.split('/')[-1].split('.')[0]:
+            sourceLandmarkLjson = lnd
+    exportPath = os.path.join(exportPathRoot, f"subject_{i:02d}", sourceObjFile[0])
+    if None in [templateObj, templateLandmarkPkl, sourceObj, sourceLandmarkLjson, exportPath]:
+        print('Issue :', sourceObjPath)
+        continue
+    print(exportPath)
+    meshRegistration(templateObj, templateLandmarkPkl, sourceObj, sourceLandmarkLjson, exportPath)
+    s = 1
 
 # import open3d as o3d
 # print(o3d)
@@ -14,27 +61,27 @@ from menpo3d.correspond import nicp
 # y = x.crop(bbox)
 # o3d.io.write_triangle_mesh('Test/1.obj', y)
 
-template = m3io.import_mesh('Data/MICCFlorence/BFM/bfm_model_front.obj')
-# template2 = m3io.import_mesh('template_ganfit_crop.obj')
-
-template_lms = np.array(mio.import_pickle('Data/MICCFlorence/BFM/landmark_ids_bfm.pkl'))
-# template_lms2 = np.array(mio.import_pickle('landmark_ids_ganfit_crop.pkl'))
-
-template.landmarks['LJSON'] = PointCloud(template.points[template_lms])
-# template2.landmarks['LJSON'] = PointCloud(template2.points[template_lms2])
-
-
-source = m3io.import_mesh('Data/MICCFlorence/Reconstruction/GroundTruth/subject_01/110920150452.obj')
-source.landmarks['LJSON'] = m3io.import_landmark_file('Data/MICCFlorence/MICC_landmarks/subject_01/Model/frontal1/obj/'
-                                                      '110920150452.ljson')['LJSON']
-
-
-lm_align = AlignmentSimilarity(source.landmarks['LJSON'], template.landmarks['LJSON']).as_non_alignment()
-source = lm_align.apply(source)
-# m3io.export_mesh(source, 'Test/bfm_aligned.obj')
-
-result = nicp.non_rigid_icp(template, source, landmark_group='LJSON', verbose=True)
-m3io.export_mesh(result, 'Data/Test/110920150452_reg.obj')
-
-
-s = 1
+# template = m3io.import_mesh('Data/MICCFlorence/BFM/bfm_model_front.obj')
+# # template2 = m3io.import_mesh('template_ganfit_crop.obj')
+#
+# template_lms = np.array(mio.import_pickle('Data/MICCFlorence/BFM/landmark_ids_bfm.pkl'))
+# # template_lms2 = np.array(mio.import_pickle('landmark_ids_ganfit_crop.pkl'))
+#
+# template.landmarks['LJSON'] = PointCloud(template.points[template_lms])
+# # template2.landmarks['LJSON'] = PointCloud(template2.points[template_lms2])
+#
+#
+# source = m3io.import_mesh('Data/MICCFlorence/Reconstruction/GroundTruth/subject_01/110920150452.obj')
+# source.landmarks['LJSON'] = m3io.import_landmark_file('Data/MICCFlorence/MICC_landmarks/subject_01/Model/frontal1/obj/'
+#                                                       '110920150452.ljson')['LJSON']
+#
+#
+# lm_align = AlignmentSimilarity(source.landmarks['LJSON'], template.landmarks['LJSON']).as_non_alignment()
+# source = lm_align.apply(source)
+# # m3io.export_mesh(source, 'Test/bfm_aligned.obj')
+#
+# result = nicp.non_rigid_icp(template, source, landmark_group='LJSON', verbose=True)
+# m3io.export_mesh(result, 'Data/Test/110920150452_reg.obj')
+#
+#
+# s = 1
