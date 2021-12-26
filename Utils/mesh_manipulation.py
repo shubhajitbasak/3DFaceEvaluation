12976f182a92042d4d787651f442341f9e1871f5
shubhajitbasak <shubhajit.basak@yahoo.com>
2021-12-26 21:02:08 +0000

    Dec 26

--- Utils/mesh_manipulation.py
+++ Utils/mesh_manipulation.py
@@ -5,6 +5,6 @@ from menpo.shape import PointCloud
 
 template = m3io.import_mesh('/mnt/sata/data/NowDataset/NoW_Dataset/ModelOutput/SwinBase/'
                             'FaMoS_180424_03335_TA/multiview_expressions/IMG_0053.obj')
-template_lms = np.array(mio.import_pickle('Data/MICCFlorence/BFM/landmark_ids_bfm.pkl'))
+template_lms = np.array(mio.import_pickle('../Data/MICCFlorence/BFM/landmark_ids_bfm.pkl'))
 template.landmarks['LJSON'] = PointCloud(template.points[template_lms])
 print('tst')
